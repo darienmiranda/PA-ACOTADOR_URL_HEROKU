a1495d15620b3e6ecddb188544cf0484fd0ab866
darienmiranda <darienmiranda72@gmail.com>
2022-03-17 18:41:20 -0500

    CONECTADO CON HEROKU

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,redirect
+import mysql.connector as mysql
+import string
+import random
+
+
+app = Flask(__name__)
+
+db = mysql.connect(
+    host="academia.c1mebdhdxytu.us-east-1.rds.amazonaws.com",
+    user="p1",
+    password="ALrUBIaLYcHR",
+    database="p1"
+)
+cursor = db.cursor()
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+@app.route("/create", methods=["GET", "POST"])
+def createShortener():
+    
+    Max_caracters = 4
+    
+    short =(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(Max_caracters)))
+    if request.method == 'POST':
+        url=request.form['Url']
+        sql= "INSERT INTO datos (short_url,large_url) VALUES (%s,%s)"
+        val = (short,url)
+        cursor.execute(sql,val)
+        db.commit()
+    
+    return render_template("shorteners/create.html", url=url,short=short)
+
+@app.get("/short/<shortened>")
+def redirection(shortened):
+    sql = "SELECT large_url FROM datos WHERE short_url = %(short_url)s"
+    cursor.execute(sql,{'short_url':shortened})
+    result = cursor.fetchone()
+    return redirect(result[0])
+app.run(debug=True)
